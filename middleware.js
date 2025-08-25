9c6a0afb3bbd3fb847ffb55685a6e99ab4d4c9c2
A0206082 <a0.206082@gmail.com>
2025-08-25 08:10:41 +0300

    fix v.001

--- middleware.js
+++ middleware.js
@@ -2,6 +2,8 @@
 import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
+export const runtime = 'nodejs';
+
 export async function middleware(req) {
     const token = req.cookies.get('auth_token')?.value;
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
